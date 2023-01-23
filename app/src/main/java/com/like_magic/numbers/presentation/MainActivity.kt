81c5e77d9f44a7460ca5c4288d5fd6fc89b7dc3e
LikeMagic85 <like-magic@yandex.ru>
2023-01-23 21:06:14 +0300

    Initial commit

---
+++ app/src/main/java/com/like_magic/numbers/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.like_magic.numbers.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.like_magic.numbers.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
