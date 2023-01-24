6d722d140bb220bfb06a394785a22c00c8dc20a2
LikeMagic85 <like-magic@yandex.ru>
2023-01-24 13:49:23 +0300

    Choose level fragment, Game Fragment, GameFinishedFragment navigation is done

--- app/src/main/java/com/like_magic/numbers/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/like_magic/numbers/presentation/GameFinishedFragment.kt
@@ -1,33 +1,73 @@
 package com.like_magic.numbers.presentation
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.like_magic.numbers.databinding.FragmentGameFinishedBinding
+import com.like_magic.numbers.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
 
+    private lateinit var gameResult: GameResult
     private var _binding: FragmentGameFinishedBinding? = null
     private val binding: FragmentGameFinishedBinding
         get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View{
         _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+               retryGame()
+            }
+        })
+        binding.buttonRetry.setOnClickListener{
+            retryGame()
+        }
+    }
 
+    private fun parseArgs(){
+        gameResult = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireArguments().getParcelable(KEY_GAME_RESULT, GameResult::class.java)?: throw RuntimeException("result == null")
+        }else {
+            requireArguments().getParcelable(KEY_GAME_RESULT)?: throw RuntimeException("result == null")
+        }
+    }
+
+    private fun retryGame(){
+        requireActivity().supportFragmentManager.popBackStack(GameFragment.NAME, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+
+        private const val KEY_GAME_RESULT = "result"
+        fun newInstance(gameResult: GameResult):GameFinishedFragment {
+            return GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(KEY_GAME_RESULT, gameResult)
+                }
+            }
+        }
     }
 }
